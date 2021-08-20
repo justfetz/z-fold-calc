4d488fecef93537768a2ae0409c11eeb40855110
Jason Fetzer <jasonmfetzer@gmail.com>
2021-08-20 11:57:38 -0500

    first commit on console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zfold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
